784ad3ef85f5e594a4de33145b2f7f98ec03ec93
Céline Godichon <celine.godichon@gmail.com>
2019-10-30 18:24:17 +0100

    codesniffer fixed

--- src/Model/MonsterManager.php
+++ src/Model/MonsterManager.php
@@ -69,7 +69,8 @@ VALUES (:name, :description, :picture, :level, :score)");
 
         // prepared request
         $statement = $this->pdo->prepare("UPDATE $this->table 
-SET `name` = :name, `description` = :description, `picture` = :picture, `level` = :level, `score` = :score WHERE id=:id");
+            SET `name` = :name, `description` = :description, `picture` = :picture, `level` = :level, `score` = :score 
+            WHERE id=:id");
         $statement->bindValue('id', $monster['id'], \PDO::PARAM_INT);
         $statement->bindValue('name', $monster['name'], \PDO::PARAM_STR);
         $statement->bindValue('description', $monster['description'], \PDO::PARAM_STR);
