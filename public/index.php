2070aab3bf2620c436edf4df7372721fd1c4685c
Anne Quiedeville <anne.quiedeville@orange.fr>
2019-11-24 18:47:58 +0100

    Heroku deploy

--- public/index.php
+++ public/index.php
@@ -7,7 +7,10 @@
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config/debug.php';
-require_once __DIR__ . '/../config/db.php';
+$env = getenv('ENV');
+if ($env === false) {
+    require_once __DIR__ . '/../config/debug.php';
+    require_once __DIR__ . '/../config/db.php';
+}
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
